a55b0fa13e549f5cf3bff3b639353c08ca67be5f
vahapmete <vahapmt@gmail.com>
2020-10-07 16:48:04 +0300

    Product Add page is modified

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DataAccess.Contcrete.EntityFramework.Contexts;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
@@ -14,6 +15,20 @@ namespace WebApplication1.Controllers
         {
             var values = db.TblProducts.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Product product)
+        {
+            db.TblProducts.Add(product);
+            db.SaveChanges();
+            return View();
         }
     }
 }
